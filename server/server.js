e863d18a6eb44d3abfe65bb9081e64f8a3d2051c
Martin Shell <mshell@alum.mit.edu>
2018-05-15 11:12:38 -0400

    Add connect and disonnect events

--- server/server.js
+++ server/server.js
@@ -1,20 +1,27 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
+io.on('connection', (socket) => {
+  console.log('New user  connected');
+});
+io.on('connection', (socket) => {
+  console.log('New user  connected');
 
-// app.get('/', (req, res) => {
-//   res.sendFile('/index.html');
-// })
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
 
-// console.log(__dirname+ '/../public');
-// console.log(publicPath);
-
-
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Started up at port ${port}`);
 });
