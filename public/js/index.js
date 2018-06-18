6ce24989093b9d1410f578d37dede36f1324cdec
Martin Shell <mshell@alum.mit.edu>
2018-06-18 18:57:42 -0400

    Style updates

--- public/js/index.js
+++ public/js/index.js
@@ -17,38 +17,45 @@ socket.on('newMessage', function(message) {
   li.text(`${message.from}: ${message.text}`);
 });
 
-socket.on('newLocationMessage', function (message) {
+socket.on('newLocationMessage', function(message) {
   var li = jQuery('<li></li');
   var a = jQuery('<a target="blank">My CurrentLocation</a>');
   li.text(`${message.from}: `);
-  a.attr('href',message.url);
+  a.attr('href', message.url);
   li.append(a);
   jQuery('#messages').append(li);
 
 })
 jQuery('#message-form').on('submit', function(e) {
   e.preventDefault();
+  messageTextbox = jQuery('[name=message]');
   socket.emit('createMessage', {
       from: 'User',
-      text: jQuery('[name=message]').val()
+      text: messageTextbox.val()
     },
     function() {
-
+      messageTextbox.val('');
     });
 });
 
 var locationButton = jQuery('#send-location');
 locationButton.on('click', function() {
-if (!navigator.geolocation) {
-  return alert('Geolocation not supported by your browser.');
-}
-navigator.geolocation.getCurrentPosition(function(position) {
-  // console.log(position);
-   socket.emit('createLocationMessage',
- {latitude: position.coords.latitude,
- longitude: position.coords.longitude});
-  },
-  function() {
-    alert('Unable to fetch your location')
-  });
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser.');
+  }
+
+locationButton.attr('disabled', 'disabled').text('Sending location...');
+
+  navigator.geolocation.getCurrentPosition(function(position) {
+      // console.log(position);
+      socket.emit('createLocationMessage', {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      });
+      locationButton.removeAttr('disabled').text('Send location');
+    },
+    function() {
+      alert('Unable to fetch your location');
+      locationButton.removeAttr('disabled').text(sendLocation);
+    });
 })
