f3456d7f0ab37c97c5ad075bda549579b002d95f
Martin Shell <mshell@alum.mit.edu>
2018-06-20 08:59:38 -0400

    add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -10,21 +10,32 @@ socket.on(('disconnect'), function() {
 
 socket.on('newMessage', function(message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
 
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
 
-  li.text(`${message.from}: ${formattedTime} ${message.text}`);
 });
 
 socket.on('newLocationMessage', function(message) {
-  var li = jQuery('<li></li');
-  var a = jQuery('<a target="blank">My CurrentLocation</a>');
-  var formattedTime = moment().format('h:mm a ');
-  li.text(`${message.from}: ${formattedTime}`);
-  a.attr('href', message.url);
-  li.append(a);
-  jQuery('#messages').append(li);
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, {
+    from: message.from,
+    createdAt: formattedTime,
+    url: message.url
+  })
+  // var li = jQuery('<li></li');
+  // var a = jQuery('<a target="blank">My CurrentLocation</a>');
+  // var formattedTime = moment().format('h:mm a ');
+  // li.text(`${message.from}: ${formattedTime}`);
+  // a.attr('href', message.url);
+  // li.append(a);
+  jQuery('#messages').append(html);
 
 })
 jQuery('#message-form').on('submit', function(e) {
@@ -45,7 +56,7 @@ locationButton.on('click', function() {
     return alert('Geolocation not supported by your browser.');
   }
 
-locationButton.attr('disabled', 'disabled').text('Sending location...');
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
 
   navigator.geolocation.getCurrentPosition(function(position) {
       // console.log(position);
